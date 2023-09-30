65474e54a198121f794507f2805e0668082205a6
Mário Neto <marioo.netoo3@gmail.com>
2023-09-30 14:44:49 -0300

    heranca pt 01 e pt 02

---
+++ introducao/javacore/Hheranca/dominio/Funcionario.java
@@ -0,0 +1,19 @@
+package maratonajava.introducao.javacore.Hheranca.dominio;
+
+public class Funcionario extends Pessoa{
+    private double salario;
+
+    @Override
+    public void imprime() {
+        super.imprime();
+        System.out.println("Salário: R$"+ this.salario);
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
